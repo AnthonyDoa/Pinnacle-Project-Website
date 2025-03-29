044f7cd21d46fb8f5184c1f8006c2e3747464f0d
AnthonyDoan <134167391+AnthonyDoa@users.noreply.github.com>
2025-03-29 18:19:17 -0400

    Create Qoute.jsx

---
+++ src/Qoute.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Qoute = () => {
+  return (
+    <div className="flex flex-col md:flex-row items-center p-10 bg-black text-white">
+     
+      <div className="text-center">
+        <h1 className="text-3xl font-bold">The Meaning Behind The Pinnacle Project</h1>
+        <p className="mt-4 text-lg">
+        "The Pinnacle Project is a community charity based in the Municipality of Missisauga, ON, dedicated to making a positive impact in our community. Our mission is to provide support and resources to those in need, and to create a better future for all. Through our various programs, initiatives, and camapigns, we strive to bring hope and relief to those facing adversity.
+
+With a passionate team of high school students from John Fraser Secondary School and a strong network of partners, we are committed to driving meaningful change and fostering a culture of compassion and generosity. Join us in our mission to build a brighter, more inclusive society for everyone."
+        </p>
+        <div className=" mt-6">
+          <div className="text-center">
+            <h2 className="font-semibold">Co-Founders</h2>
+            <p className="text-white">Meesam Ali & Cody Doan </p>
+          </div>
+         
+          
+        </div>
+      </div>
+    </div>
+
+
+
+  );
+};
+
+export default Qoute;
