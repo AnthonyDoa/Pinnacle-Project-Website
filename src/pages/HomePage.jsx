b0704a8419d53d9c0fc3b837252afa54168b03f3
AnthonyDoan <134167391+AnthonyDoa@users.noreply.github.com>
2025-03-29 18:22:59 -0400

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,22 @@
 import Hero from '../components/Hero';
 import HomeCards from '../components/HomeCards';
+import Ourteam from '../components/Ourteam';
+import Qoute from '../components/Qoute';
 import JobListings from '../components/JobListings';
 import ViewAllJobs from '../components/ViewAllJobs';
+import Voluteer from '../components/Voluteer';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <HomeCards />
+      <Qoute />
       <JobListings isHome={true} />
       <ViewAllJobs />
+      <Ourteam />
+      <Voluteer />
+
     </>
   );
 };
