569f95bffaf70e5f80f29cf33d2cc8c421db4fbc
AnthonyDoan <134167391+AnthonyDoa@users.noreply.github.com>
2025-04-21 15:20:22 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ import logo from '../assets/images/logo.png';
 const Navbar = () => {
   const linkClass = ({ isActive }) =>
     isActive
-      ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-      : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+      ? 'bg-black text-white hover:bg-gray-400 hover:text-white rounded-md px-3 py-2'
+      : 'text-white hover:bg-gray-400 hover:text-white rounded-md px-3 py-2';
 
   return (
-    <nav className='sticky top-0 z-50 bg-indigo-700 border-b border-indigo-500'>
+    <nav className='sticky top-0 z-50 bg-black border-b border-black'>
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
         <div className='flex h-20 items-center justify-between'>
           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
@@ -25,9 +25,6 @@ const Navbar = () => {
                 </NavLink>
                 <NavLink to='/jobs' className={linkClass}>
                   Campiagns
-                </NavLink>
-                <NavLink to='/add-job' className={linkClass}>
-                  Add Campaigns
                 </NavLink>
                 <NavLink to='/OurteamFull' className={linkClass}>
                   Our Team
