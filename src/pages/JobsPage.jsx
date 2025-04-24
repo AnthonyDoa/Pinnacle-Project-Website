e58b8edb97af38ba65c0d7247848aa2c6796b5d2
AnthonyDoan <134167391+AnthonyDoa@users.noreply.github.com>
2025-04-23 20:27:36 -0400

    Update JobsPage.jsx

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -19,7 +19,7 @@ const JobsPage = ({
 <div
   className="hero-container flex flex-col items-center justify-start pt-20"
   style={{
-    backgroundImage: 'url("https://images.unsplash.com/photo-1744762561513-6691932920fb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D")',
+    backgroundImage: 'url("https://images.unsplash.com/photo-1564577159902-0494decdb93e?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     minHeight: '60vh', /* Adjust this value as needed */
@@ -31,7 +31,7 @@ const JobsPage = ({
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, ease: 'easeInOut' }}
-        className="text-3xl sm:text-5xl lg:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-500"
+        className="text-3xl sm:text-5xl lg:text-7xl font-bold text-transparent bg-clip-text bg-black"
       >
         Goals & Campaigns
       </motion.h1>
@@ -42,7 +42,7 @@ const JobsPage = ({
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, ease: 'easeInOut' }}
-        className=" text-xl sm:text-3xl lg:text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-500"
+        className=" text-xl sm:text-3xl lg:text-3xl font-bold text-transparent bg-clip-text bg-black"
       >
         Make a difference in your community and learn about our goals and
         campaigns in helping families and individuals in the Mississauga area
@@ -59,7 +59,7 @@ const JobsPage = ({
 </div>
       <div className="flex flex-col md:flex-row items-center p-10 bg-gradient-to-br from-black to-gray-500 text-white">
       <img
-        src="/assets/images/Banner.jpeg"
+        src="https://www.smallbusinesscoach.org/wp-content/uploads/2022/05/marketing-campaigns.jpg"
         alt="Who we are"
         className="w-96 h-auto object-cover rounded-lg shadow-lg md:mr-10"
       />
@@ -69,33 +69,7 @@ const JobsPage = ({
         Join The Pinnacle Project in our monthly campaigns, each with a specific focus. From education to the environment, health, and community well-being, we tackle different goals each month. These initiatives are tailored to address specific needs within our community, making it easy for you to engage with causes that matter most to you. By participating in these impactful campaigns, you contribute to the overall progress of our community. Whether you're passionate about learning, sustainability, or health, there's a monthly opportunity for you to make a meaningful difference. Let's work together, one focused campaign at a time, to create positive and lasting change within our community.
         </p> </div></div>
       <JobListings />
-       <div className="flex flex-col md:flex-row items-center p-10  bg-gradient-to-br from-black to-gray-500 text-white">
-            {/* Text Section - Now on the Left */}
-            <div className="text-left max-w-xl md:mr-10">
-              <h1 className="text-3xl font-bold">Getting Involved & Volunteering</h1>
-              <p className="mt-4 text-lg">
-              Unlock meaningful experiences by volunteering with The Pinnacle Project! High school students can contribute their time to make a positive impact on our community. Gain valuable volunteer hours while making a difference in the lives of others. As a token of appreciation, participants receive a reference letter highlighting their dedication and contributions. Join us in fostering positive change and building a better future together. Enrich your high school journey with purposeful volunteering at The Pinnacle Project.
-              </p>
-      
-             
-      
-              {/* Link Button */}
-              <Link
-               to = "/Voluteer&Parthership"
-                
-                className="mt-6 inline-block bg-white text-black font-semibold px-6 py-3 rounded-xl shadow-lg hover:bg-gray-200 transition"
-              >
-                Learn More
-              </Link>
-            </div>
-      
-            {/* Image Section - Now on the Right */}
-            <img
-              src={bannerImage}
-              alt="Who we are"
-              className="w-96 h-auto object-cover rounded-lg shadow-lg md:ml-10"
-            />
-          </div>
+       
           <div className="bg-black text-white min-h-screen">
   <ImageTrack />
 </div>
