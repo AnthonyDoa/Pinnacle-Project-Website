9ec06fc1d1cc6355b4f02150e9cdc462bb6dd6e8
AnthonyDoan <134167391+AnthonyDoa@users.noreply.github.com>
2025-04-24 19:02:46 -0400

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,6 @@ import Hero from '../components/Hero';
 import HomeCards from '../components/HomeCards';
 import Ourteam from '../components/Ourteam';
 import Qoute from '../components/Qoute';
-import JobListings from '../components/JobListings';
-import ViewAllJobs from '../components/ViewAllJobs';
 import Voluteer from '../components/Voluteer';
 import Partherships from '../components/Partherships';
 import Contact from '../components/Contact';
