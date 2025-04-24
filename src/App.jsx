a2e83447fdfe6361b7bbbe15e89574944b55488e
AnthonyDoan <134167391+AnthonyDoa@users.noreply.github.com>
2025-04-23 20:37:02 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,64 +6,20 @@ import {
 } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
-import JobsPage from './pages/JobsPage';
+import CampiagnsPage from './pages/CampiagnsPage';
 import NotFoundPage from './pages/NotFoundPage';
-import JobPage, { jobLoader } from './pages/JobPage';
-import AddJobPage from './pages/AddJobPage';
-import EditJobPage from './pages/EditJobPage';
 import OurteamFull from './pages/OurteamFull';
 import VolunteeringPartnerships from './pages/VolunteeringPartnerships';
 import Blog from './pages/Blog';
 import BlogPost from './pages/BlogPost';
 const App = () => {
-  // Add New Job
-  const addJob = async (newJob) => {
-    const res = await fetch('/api/jobs', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newJob),
-    });
-    return;
-  };
 
-  // Delete Job
-  const deleteJob = async (id) => {
-    const res = await fetch(`/api/jobs/${id}`, {
-      method: 'DELETE',
-    });
-    return;
-  };
-
-  // Update Job
-  const updateJob = async (job) => {
-    const res = await fetch(`/api/jobs/${job.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(job),
-    });
-    return;
-  };
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
-        <Route path='/jobs' element={<JobsPage />} />
-        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route
-          path='/edit-job/:id'
-          element={<EditJobPage updateJobSubmit={updateJob} />}
-          loader={jobLoader}
-        />
-        <Route
-          path='/jobs/:id'
-          element={<JobPage deleteJob={deleteJob} />}
-          loader={jobLoader}
-        />
+        <Route path='/jobs' element={<CampiagnsPage />} />
         <Route path='*' element={<NotFoundPage />} />
         <Route path='/OurteamFull' element={<OurteamFull />} />
         <Route path='/VolunteeringPartnerships' element={<VolunteeringPartnerships />} />
